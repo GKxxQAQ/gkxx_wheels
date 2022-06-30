74544e971d642518a1d028de7378c1a58b2cf11d
GKxxQAQ <2863746329@qq.com>
2022-06-30 19:03:16 +0800

    remove those useless things

--- tests/io_utility/test.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdio>
-
-int main() {
-    float x;
-    scanf("%f", &x);
-    printf("%.16f\n", x);
-    return 0;
-}
--- tests/range/transform.cpp
+++
@@ -1,9 +0,0 @@
-#include "wheels/dynarray.h"
-#include "wheels/range.h"
-#include <iostream>
-
-int main() {
-    using namespace gkxx;
-    print(Dynarray<int>(Range(1, 10, 3)));
-    return 0;
-}
